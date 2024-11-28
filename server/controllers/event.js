fefd66811dd43c6fb19ed9fefc5667e2153ef7dd
Giougt <alexandre18golf@gmail.com>
2024-11-28 17:23:54 +0100

    add event/routes + start controllers/event

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,1 +1,10 @@
 const Event = require("../models/event");  
+
+exports.cget = async (req, res, next) => {
+    try {
+      const events = await Event.findAll();
+      res.json(events);
+    } catch (error) {
+      next(error);
+    }
+  };
