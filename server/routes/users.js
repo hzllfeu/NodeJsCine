11405921eee7a7fc8d94384a6af7fbcf266fea74
louismouchon <louis.mouchon@live.com>
2024-12-22 22:09:46 +0100

    Route pour permissions admin

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,4 +11,8 @@ router.get("/users/:id", UserController.get);
 router.patch("/users/:id", UserController.patch);
 router.delete("/users/:id", checkAuth, UserController.delete);
 
+router.post("/users", authenticate, requireRole("admin"), filmController.post);
+router.patch("/users/:id", authenticate, requireRole("admin"), filmController.patch);
+router.delete("/users/:id", authenticate, requireRole("admin"), filmController.delete);
+
 module.exports = router;  
